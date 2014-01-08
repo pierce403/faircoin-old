4f2ae799f12c19c853b160bb5307fae59bf6e571
Dean Pierce <pierce403@gmail.com>
2014-01-07 21:42:39 -0800

    setting merkel root etc

--- src/main.cpp
+++ src/main.cpp
@@ -1981,7 +1981,7 @@ bool LoadBlockIndex(bool fAllowNew)
         pchMessageStart[1] = 0xc0;
         pchMessageStart[2] = 0xb8;
         pchMessageStart[3] = 0xdb;
-        hashGenesisBlock = uint256("0x");
+        hashGenesisBlock = uint256("0x295e0e42a86a4a04bdb0f7794c4d6080fe5b53c6f6897ac6dca1a61dbdbe7bc1");
     }
 
     //
@@ -2021,14 +2021,14 @@ bool LoadBlockIndex(bool fAllowNew)
         if (fTestNet)
         {
             block.nTime    = 1389150298;
-            block.nNonce   = 0;
+            block.nNonce   = 442710;
         }
 
         //// debug print
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x"));
+        assert(block.hashMerkleRoot == uint256("0xb4640dde645cd0a85930bb4048cfe364f2d2059e9be9f555c1176d67379eaaaa"));
 
         // If genesis block hash does not match, then generate new genesis hash.
         if (true && block.GetHash() != hashGenesisBlock)
